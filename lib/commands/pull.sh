4e4e1c97f1effb29775df6ad1c04395d73fb07ae
Michael Rommel <michael.rommel@siemens-healthineers.com>
2023-05-18 19:12:43 +0200

    fix: missing commands

--- lib/commands/pull.sh
+++ lib/commands/pull.sh
@@ -29,25 +29,6 @@ pull() {
   return "$EX_SUCCESS"
 }
 
-run_post_pull_cmd() {
-  while [[ $# -gt 0 ]]; do
-    local git_repo=$1
-    if is_github_shorthand "$git_repo"; then
-      git_repo="https://github.com/$git_repo.git"
-    fi
-    local castle
-    castle=$(repo_basename "$git_repo")
-    shift
-    local repo="$repos/$castle"
-    if [[ ! -f $repo/bootstrap/post-pull.sh ]]; then
-      continue;
-    else
-      "$repo/bootstrap/post-pull.sh"
-    fi
-  done
-  return "$EX_SUCCESS"
-}
-
 symlink_new_files() {
   local updated_castles=()
   while [[ $# -gt 0 ]]; do
@@ -69,4 +50,42 @@ symlink_new_files() {
   done
   ask_symlink "${updated_castles[@]}"
   return "$EX_SUCCESS"
+}
+
+run_post_pull_cmd() {
+  while [[ $# -gt 0 ]]; do
+    local git_repo=$1
+    if is_github_shorthand "$git_repo"; then
+      git_repo="https://github.com/$git_repo.git"
+    fi
+    local castle
+    castle=$(repo_basename "$git_repo")
+    shift
+    local repo="$repos/$castle"
+    if [[ ! -f $repo/bootstrap/post-pull.sh ]]; then
+      continue;
+    else
+      "$repo/bootstrap/post-pull.sh"
+    fi
+  done
+  return "$EX_SUCCESS"
+}
+
+# Convert username/repo into https://github.com/username/repo.git
+is_github_shorthand() {
+  if [[ ! $1 =~ \.git$ && $1 =~ ^([0-9A-Za-z-]+/[0-9A-Za-z_\.-]+)$ ]]; then
+    return 0
+  fi
+  return 1
+}
+
+# Get the repo name from an URL
+repo_basename() {
+if [[ $1 =~ ^[^/:]+: ]]; then
+  # For scp-style syntax like '[user@]host.xz:path/to/repo.git/',
+  # remove the '[user@]host.xz:' part.
+  basename "${1#*:}" .git
+else
+  basename "$1" .git
+fi
 }
