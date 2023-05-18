36fe237bd29d59be3c699f07c05e3125f33ee8d8
Michael Rommel <michael.rommel@siemens-healthineers.com>
2023-05-18 18:38:03 +0200

    refactor: change to go version manager

--- home/.path.d/40_go.sh
+++ home/.path.d/40_go.sh
@@ -3,3 +3,10 @@
 # Go location
 GOPATH=$(readlink -f "${HOME}/software")/go
 export GOPATH
+
+# look for go version manager
+if [[ -d "${HOME}/.gobrew/bin" && ! "$PATH" == *${HOME}/.gobrew/bin* ]]; then
+	# path has not yet been added
+	export PATH="${HOME}/.gobrew/current/bin:${HOME}/.gobrew/bin:${PATH}"
+	export GOROOT="${HOME}/.gobrew/current/go"
+fi
